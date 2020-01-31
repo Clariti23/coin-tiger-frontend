5e9a5daaa64c05566edabcff9442dc3e9b0e99d6
Farmwell <g.ferowich@gmail.com>
2020-01-31 07:38:19 -0500

    set the state after api fetch in cointable

--- coin-tiger-client/src/CoinTable.js
+++ coin-tiger-client/src/CoinTable.js
@@ -4,22 +4,21 @@ export default class CoinTable extends Component {
   CoinGecko = require("coingecko-api");
   CoinGeckoClient = new this.CoinGecko();
 
-  //   function createData(coin, Price, dailyChange, mktCap) {
-  //     return { coin, price, dailyChange, mktCap };
-  //   }
+  state = {
+    coins: []
+  };
 
   componentDidMount() {
     this.CoinGeckoClient.coins
       .markets({
         vs_currency: "usd",
         order: "market_cap_desc",
-        per_page: 10,
+        per_page: 100,
         page: 1,
         sparkline: false,
         price_change_percentage: "24h"
       })
-
-      .then(data => console.log(data));
+      .then(data => this.setState({ coins: data.data }));
   }
 
   render() {
