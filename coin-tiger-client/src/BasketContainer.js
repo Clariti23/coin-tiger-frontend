aa6327af87d8b2022a65e6d039fb0e0ed03ae173
Farmwell <g.ferowich@gmail.com>
2020-02-07 18:50:39 -0500

    skeleton for basket value calculation

--- coin-tiger-client/src/BasketContainer.js
+++ coin-tiger-client/src/BasketContainer.js
@@ -13,12 +13,28 @@ export default class BasketContainer extends Component {
 
     fetch(this.BASKET_API)
       .then(response => response.json())
-      .then(data => this.basketsFilter(data));
+      .then(data => this.basketCalculate(data));
   }
 
-  basketsFilter = data => {
+  basketCalculate = data => {
     let userBasket = data.filter(b => b.user_id === parseInt(this.state.UID));
-    this.setState({ basket: userBasket }, () => console.log);
+    this.setState({ basket: userBasket }, () => console.log(this.state));
+
+    // //Store Coin Components
+    // let coinOneSymbol = this.state.basket.coinOne;
+    // let coinTwoSymbol = this.state.basket.coinTwo;
+    // let coinThreeSymbol = this.state.basket.coinThree;
+    // let coinFourSymbol = this.state.basket.coinFour;
+    // let coinFiveSymbol = this.state.basket.coinFive;
+
+    // //Initial quantites of each coin
+    // let coinOneQ = this.state.basket.coin_1_q;
+    // let coinTwoQ = this.state.basket.coin_2_q;
+    // let coinThreeQ = this.state.basket.coin_3_q;
+    // let coinFourQ = this.state.basket.coin_4_q;
+    // let coinFiveQ = this.state.basket.coin_5_q;
+
+    // let basketValue = this.state.
   };
 
   render() {
