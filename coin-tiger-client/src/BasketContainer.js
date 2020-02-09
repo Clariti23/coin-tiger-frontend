085ad9574d573ae43983fce5f68c3877e8d90228
Farmwell <g.ferowich@gmail.com>
2020-02-09 11:58:51 -0500

    basketContainer renders dummy data for basket

--- coin-tiger-client/src/BasketContainer.js
+++ coin-tiger-client/src/BasketContainer.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Basket from "./Basket";
+
 export default class BasketContainer extends Component {
   state = {
-    basket: [],
+    baskets: [],
     UID: null
   };
   BASKET_API = "http://localhost:3000/baskets";
@@ -13,18 +14,22 @@ export default class BasketContainer extends Component {
 
     fetch(this.BASKET_API)
       .then(response => response.json())
-      .then(data => this.basketCalculate(data));
+      .then(data => this.basketsFilter(data));
   }
 
-  basketCalculate = data => {
-    let userBasket = data.filter(b => b.user_id === parseInt(this.state.UID));
-    this.setState({ basket: userBasket }, () => console.log(this.state.basket));
+  basketsFilter = data => {
+    let userBaskets = data.filter(b => b.user_id === parseInt(this.state.UID));
+    this.setState({ baskets: userBaskets }, () =>
+      console.log(this.state.baskets)
+    );
   };
 
   render() {
     return (
       <div>
-        <Basket basket={this.state.basket}></Basket>
+        {this.state.baskets.map(basket => (
+          <Basket key={basket.id} basket={basket}></Basket>
+        ))}
       </div>
     );
   }
