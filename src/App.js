6eb3e88b76591169192fac9207e71e02d8394444
Farmwell <g.ferowich@gmail.com>
2020-02-21 14:38:41 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -8,18 +8,17 @@ const App = props => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   let handleUserLogIn = user => {
-    console.log(user);
-    // if (user) {
-    //   let userID = user.user.user.id;
-    //   setCurrentUser(user.user.user.name);
-    //   setCurrentUserId(userID);
-    //   setLoggedIn(true);
-    //   localStorage.setItem("name", user.user.user.name);
-    //   localStorage.setItem("isLoggedIn", true);
-    //   localStorage.setItem("UID", userID);
-    // } else {
-    //   alert("Invalid username or password");
-    // }
+    if (user.jwt) {
+      let userID = user.user.user.id;
+      setCurrentUser(user.user.user.name);
+      setCurrentUserId(userID);
+      setLoggedIn(true);
+      localStorage.setItem("name", user.user.user.name);
+      localStorage.setItem("isLoggedIn", true);
+      localStorage.setItem("UID", userID);
+    } else {
+      alert("Invalid username or password");
+    }
   };
 
   let handleLogout = () => {
