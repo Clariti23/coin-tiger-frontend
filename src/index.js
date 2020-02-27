757f67ebdd0b053aefa40f1d9f0c35ac8a83fd47
Farmwell <g.ferowich@gmail.com>
2020-02-27 08:38:04 -0500

    update theme, colors

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import { MuiThemeProvider } from "material-ui/styles";
-// import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: {
-//       main: "#ff8f00"
-//     },
-//     secondary: {
-//       main: "#ffcc80"
-//     }
-//   }
-// });
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ff8f00"
+    },
+    secondary: {
+      main: "#ffcc80"
+    }
+  }
+});
 
-/* <MuiThemeProvider theme={theme}></MuiThemeProvider> */
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
